50f79e51aa31fa4468b8a71a766ab7d7c55dac23
fedyancev.d.a <fedyancev.d.a@effective-group.ru>
2022-02-04 11:44:03 +0300

    рефактор di онтейнера symfony

--- src/DI/SymfonyDiContainerInit.php
+++ src/DI/SymfonyDiContainerInit.php
@@ -2,9 +2,11 @@
 
 namespace EfTech\BookLibrary\Infrastructure\DI;
 
+use EfTech\BookLibrary\Infrastructure\DI\SymfonyDiContainerInit\CacheParams;
 use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
@@ -17,15 +19,43 @@ class SymfonyDiContainerInit
      */
     private string $path;
     private array $parameters;
+    /** Параметры отвечающие за кеширование
+     * @var CacheParams
+     */
+    private CacheParams $cacheParams;
 
     /**
      * @param string $path
      * @param array $parameters
+     * @param CacheParams|null $cacheParams
      */
-    public function __construct(string $path, array $parameters = [])
+    public function __construct(string $path, array $parameters = [], CacheParams $cacheParams = null)
     {
         $this->path = $path;
         $this->parameters = $parameters;
+        $this->cacheParams = $cacheParams ?? new CacheParams(false);
+    }
+
+    /** Фабрика  реализующая логику создания di контайнера symfony
+     *
+     * @param string $path путь до xml конфига di контайнера symfony
+     * @param array $parameters
+     * @return ContainerBuilder|ContainerInterface
+     * @throws Exception
+     */
+    public static function createContainerBuilder(string $path, array $parameters): ContainerBuilder
+    {
+        $containerBuilder = new class extends ContainerBuilder implements ContainerInterface
+        {
+        };
+        foreach ($parameters as $parameterName => $parameterValue) {
+            $containerBuilder->setParameter($parameterName, $parameterValue);
+            //$containerBuilder->setParameter('kernel.project_dir', __DIR__ . '/../../../../../');
+        }
+        $loader = new XmlFileLoader($containerBuilder, new FileLocator());
+        $loader->load($path);
+
+        return $containerBuilder;
     }
 
     /** Логика создания di контейнера symfony
@@ -34,18 +64,16 @@ class SymfonyDiContainerInit
      */
     public function __invoke(): ContainerInterface
     {
-        $containerBuilder = new class extends ContainerBuilder implements ContainerInterface
-        {
-        };
-        foreach ($this->parameters as $parameterName => $parameterValue) {
-            $containerBuilder->setParameter($parameterName, $parameterValue);
-            //$containerBuilder->setParameter('kernel.project_dir', __DIR__ . '/../../../../../');
-        }
-        $loader = new XmlFileLoader($containerBuilder, new FileLocator());
-        $loader->load($this->path);
-
+        $containerBuilder = self::createContainerBuilder($this->path, $this->parameters);
         $containerBuilder->compile();
 
+        if (true === $this->cacheParams->getPathToCacheFile()) {
+            $dumper = new PhpDumper($containerBuilder);
+            file_put_contents(
+                $this->cacheParams->getPathToCacheFile(),
+                $dumper->dump(['class' => 'EfTechBookLibraryCachedContainer'])
+            );
+        }
 
         return $containerBuilder;
     }
